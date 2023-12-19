7bd3e476f8b8a4acb20f0988bc8fb7e064a3dda2
Klim Medvedev <dater01@mail.ru>
2023-12-19 20:34:06 +0300

    -

--- calc/main.js
+++ calc/main.js
@@ -1,103 +1,224 @@
-let displayValue = '';
+// Функция priority позволяет получить 
+// значение приоритета для оператора.
+// Возможные операторы: +, -, *, /.
+// ----------------------------------------------------------------------------
+// The "priority" function allows you to 
+// get the priority of an operator. 
+// Possible operators: +, -, *, /.
 
-function appendToDisplay(value) {
-    displayValue += value;
-    document.getElementById('display').value = displayValue;
+function priority(operation) {
+    if (operation == '+' || operation == '-') {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
-function clearDisplay() {
-    displayValue = '';
-    document.getElementById('display').value = displayValue;
+// Проверка, является ли строка str числом.
+// ----------------------------------------------------------------------------
+// Checking if the string "str" contains a number.
+
+function isNumeric(str) {
+    return /^\d+(.\d+){0,1}$/.test(str);
 }
 
-function customEvaluate(expression) {
-    const operators = ['+', '-', '*', '/'];
-    const outputQueue = [];
-    const operatorStack = [];
+// Проверка, является ли строка str цифрой.
+// ----------------------------------------------------------------------------
+// Checking if the string "str" contains a digit.
 
-    const precedence = {
-        '+': 1,
-        '-': 1,
-        '*': 2,
-        '/': 2,
-    };
+function isDigit(str) {
+    return /^\d{1}$/.test(str);
+}
 
-    const isOperator = (token) => operators.includes(token);
+// Проверка, является ли строка str оператором.
+// ----------------------------------------------------------------------------
+// Checking if the string "str" contains an operator.
 
-    const shuntingYard = (tokens) => {
-        for (const token of tokens) {
-            if (!isNaN(parseFloat(token))) {
-                outputQueue.push(parseFloat(token));
-            } else if (isOperator(token)) {
-                while (operatorStack.length > 0 &&
-                    // eslint-disable-next-line max-len
-                    precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]) {
-                    outputQueue.push(operatorStack.pop());
-                }
-                operatorStack.push(token);
-            } else if (token === '(') {
-                operatorStack.push(token);
-            } else if (token === ')') {
-                while (operatorStack.length > 0 && 
-                    operatorStack[operatorStack.length - 1] !== '(') {
-                    outputQueue.push(operatorStack.pop());
-                }
-                operatorStack.pop();
+function isOperation(str) {
+    return /^[\+\-\*\/]{1}$/.test(str);
+}
+
+// Функция tokenize принимает один аргумент -- строку
+// с арифметическим выражением и делит его на токены 
+// (числа, операторы, скобки). Возвращаемое значение --
+// массив токенов.
+// ----------------------------------------------------------------------------
+// The "tokenize" function takes one argument, a string 
+// with an arithmetic expression, and divides it into 
+// tokens (numbers, operators, brackets).The return value 
+// is an array of tokens.
+
+function tokenize(str) {
+    let tokens = [];
+    let lastNumber = '';
+    for (char of str) {
+        if (isDigit(char) || char == '.') {
+            lastNumber += char;
+        } else {
+            if (lastNumber.length > 0) {
+                tokens.push(lastNumber);
+                lastNumber = '';
             }
         }
-        while (operatorStack.length > 0) {
-            outputQueue.push(operatorStack.pop());
+        if (isOperation(char) || char == '(' || char == ')') {
+            tokens.push(char);
         }
-    };
+    }
+    if (lastNumber.length > 0) {
+        tokens.push(lastNumber);
+    }
+    return tokens;
+}
 
-    const evaluateRPN = (tokens) => {
-        const stack = [];
-        for (const token of tokens) {
-            if (!isNaN(parseFloat(token))) {
-                stack.push(parseFloat(token));
-            } else if (isOperator(token)) {
-                const b = stack.pop();
-                const a = stack.pop();
-                switch (token) {
-                case '+':
-                    stack.push(a + b);
-                    break;
-                case '-':
-                    stack.push(a - b);
-                    break;
-                case '*':
-                    stack.push(a * b);
-                    break;
-                case '/':
-                    if (b === 0) {
-                        throw new Error('Division by zero');
-                    }
-                    stack.push(a / b);
-                    break;
-                default:
-                    throw new Error('Unknown operator');
-                }
+// Функция compile принимает один аргумент -- строку
+// с арифметическим выражением, записанным в инфиксной 
+// нотации, и преобразует это выражение в обратную 
+// польскую нотацию (ОПН). Возвращаемое значение -- 
+// результат преобразования в виде строки, в которой 
+// операторы и операнды отделены друг от друга пробелами. 
+// Выражение может включать действительные числа, операторы 
+// +, -, *, /, а также скобки. Все операторы бинарны и левоассоциативны.
+// Функция реализует алгоритм сортировочной станции 
+// (https://ru.wikipedia.org/wiki/Алгоритм_сортировочной_станции).
+// ----------------------------------------------------------------------------
+// The compile function takes one argument, a string with an arithmetic 
+// expression written in infix notation, and converts this expression to 
+// reverse Polish notation(RPN).The return value is the result of the 
+// conversion as a string with operators and operands separated by 
+// spaces.The expression can include real numbers, +, -, *, / operators, 
+// and brackets. All operators are binary and left associative. 
+// The function implements the Shunting yard algorithm
+// (https://en.wikipedia.org/wiki/Shunting_yard_algorithm).
+
+function compile(str) {
+    let out = [];
+    let stack = [];
+    for (token of tokenize(str)) {
+        if (isNumeric(token)) {
+            out.push(token);
+        } else if (isOperation(token)) {
+            while (stack.length > 0 &&
+            isOperation(stack[stack.length - 1]) &&
+            priority(stack[stack.length - 1]) >= priority(token)) {
+                out.push(stack.pop());
             }
+            stack.push(token);
+        } else if (token == '(') {
+            stack.push(token);
+        } else if (token == ')') {
+            while (stack.length > 0 && stack[stack.length - 1] != '(') {
+                out.push(stack.pop());
+            }
+            stack.pop();
         }
-        return stack.pop();
+    }
+    while (stack.length > 0) {
+        out.push(stack.pop());
+    }
+    return out.join(' ');
+}
+
+// Функция evaluate принимает один аргумент -- строку 
+// с арифметическим выражением, записанным в обратной 
+// польской нотации. Возвращаемое значение -- результат 
+// вычисления выражения. Выражение может включать 
+// действительные числа и операторы +, -, *, /.
+// Вам нужно реализовать эту функцию
+// (https://ru.wikipedia.org/wiki/Обратная_польская_запись#Вычисления_на_стеке).
+// ----------------------------------------------------------------------------
+// The evaluate function takes one argument, a string 
+// containing an arithmetic expression written in reverse 
+// Polish notation.The return value is the result of 
+// evaluating the expression.The expression can include 
+// real numbers and the operators +, -, *, /. 
+// You need to implement this function
+// (https://en.wikipedia.org/wiki/Reverse_Polish_notation).
+
+function evaluate(str) {
+    const stack = [];
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
     };
 
-    const tokens = expression.match(/(\d+(\.\d+)?|[+\-*/()])/g);
-    if (!tokens) {
+    const tokens = str.split(' ');
+
+    for (const token of tokens) {
+        if (!isNaN(parseFloat(token))) {
+            stack.push(parseFloat(token));
+        } else if (token in operators) {
+            const operand2 = stack.pop();
+            const operand1 = stack.pop();
+            const result = operators[token](operand1, operand2);
+            stack.push(result);
+        } else {
+            throw new Error('Invalid token: ' + token);
+        }
+    }
+
+    if (stack.length !== 1) {
         throw new Error('Invalid expression');
     }
 
-    shuntingYard(tokens);
-    return evaluateRPN(outputQueue);
+    return stack[0];
 }
 
-function calculateResult() {
-    try {
-        const result = customEvaluate(displayValue);
-        document.getElementById('display').value = result;
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
+// Функция clickHandler предназначена для обработки 
+// событий клика по кнопкам калькулятора. 
+// По нажатию на кнопки с классами digit, operation и bracket
+// на экране (элемент с классом screen) должны появляться 
+// соответствующие нажатой кнопке символы.
+// По нажатию на кнопку с классом clear содержимое экрана 
+// должно очищаться.
+// По нажатию на кнопку с классом result на экране 
+// должен появиться результат вычисления введённого выражения 
+// с точностью до двух знаков после десятичного разделителя (точки).
+// Реализуйте эту функцию. Воспользуйтесь механизмом делегирования 
+// событий (https://learn.javascript.ru/event-delegation), чтобы 
+// не назначать обработчик для каждой кнопки в отдельности.
+// ----------------------------------------------------------------------------
+// The clickHandler function is designed to handle click events 
+// on calculator buttons. When buttons with classes "digit", 
+// "operation" and "bracket" are pressed, the symbols corresponding 
+// to the pressed button should appear on the screen 
+// (element with the class "screen"). On clicking the button with 
+// the "clear" class, the contents of the screen should be cleared.
+// By clicking on the button with the "result" class, the result of 
+// the calculation of the entered expression should appear on the screen 
+// with an accuracy of two decimal places after the decimal separator (point). 
+// Implement this function. Use the event delegation mechanism 
+// (https://javascript.info/event-delegation) so as not to set a 
+// handler for each button separately.
+
+function clickHandler(event) {
+    const screen = document.querySelector('.screen')
+    const target = event.target;
+
+    if (screen.textContent === 'Error'){
+        screen.textContent = '';
+    } else if (target.classList.contains('digit') || target.classList.contains('operation') || target.classList.contains('bracket')) {
+        screen.textContent += target.textContent;
+    } else if (target.classList.contains('clear')) {
+        screen.textContent = '';
+    } else if (target.classList.contains('result')) {
+        try {
+            const result = evaluate(compile(screen.textContent));
+            screen.textContent = result;
+        } catch (error) {
+            screen.textContent = 'Error';
+        }
     }
 }
 
 
+// Назначьте нужные обработчики событий.
+// ----------------------------------------------------------------------------
+// Set event handlers.
+
+window.onload = function () {
+    const calculator = document.querySelector('.calc-container');
+    document.querySelector('.screen').textContent = '';
+    calculator.addEventListener('click', clickHandler);
+};
